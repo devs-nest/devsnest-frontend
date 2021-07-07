87acb7ee416fe33d13338fc65587339ceae1a566
ashirbad <ashirbadbehera29@gmail.com>
2021-07-07 18:34:01 +0530

    ❌ add TL VTL checks

--- src/services/weekly_todo.js
+++ src/services/weekly_todo.js
@@ -19,7 +19,8 @@ export const getStreak = async (group_id) => {
   return response.data;
 };
 
-export const saveWeeklyTodo = async (state) => {
+export const saveWeeklyTodo = async (state, isTlVtl) => {
+  if (!isTlVtl) return;
   const {
     todo_id,
     sheet_filled,
@@ -40,9 +41,11 @@ export const saveWeeklyTodo = async (state) => {
     feedback,
     todo_list,
   };
+
   console.log(state);
   const response = await axios.put(`${API_ENDPOINTS.WEEKLY_TODO}/${todo_id}`, {
     data: { id: todo_id, type: 'weekly_todos', attributes },
   });
+  // console.log({ data: { id: todo_id, type: 'weekly_todos', attributes } });
   return response;
 };
